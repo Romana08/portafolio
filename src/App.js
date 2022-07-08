2c15c795573bbee4cc4b774eab79bf4eb900d0ac
Romana08 <mortonromina@gmail.com>
2022-07-07 22:55:09 -0300

    responsive

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ function App(){
     <div className= "cont" id = 'home'>
      {/* <NavBar></NavBar> */}
      <Portada></Portada>
-     <SobreMi title= "Sobre mi" id= "Sobre Mi"></SobreMi>
+      <SobreMi title= "Sobre mi" id= "Sobre Mi"></SobreMi>
      <Skills title= "Skills" id= "skills"></Skills>
      <MyWork title= "My work"  id= "work"></MyWork>
      <Contacto title= "Contacto" id= "contacto"></Contacto>
-     
+      
     <div>
       Portafolio Web - Idea Original - Romina Morton
       </div>
